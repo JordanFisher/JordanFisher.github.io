48152820c2206ce81ad656a424d70be97f4359a5
Jordan Fisher <jordan.efisher@gmail.com>
2025-03-08 19:39:48 -0800

    Regen

--- doc_list.py
+++ doc_list.py
@@ -1,4 +1,7 @@
 gdoc_urls = [
+    (["tiny_book_on_governance_of_machine"], "https://docs.google.com/document/d/1-CifFI15IsW5fj4DvSrH9SmpKJbAELyeHTKCfdGT6rs/"),
     (["the_corpse_still_warm"], "https://docs.google.com/document/d/1VnChDrRDJVqjMwu_kHS1BzhFjpn37ryNrRWZdaRUUxc/"),
-    (["implicit_guardrails"], "https://docs.google.com/document/d/1t04E8WY-0purWxgSpSr3KFv63b5YWMY_LpwmU1NZ0cQ/"),
+    (["implicit_guardrails"], "https://docs.google.com/document/d/1YSvUWGOTsk521pFGTIoaw6o375P-Hl5yDnh15dQ9MGg/"),
+    (["by_any_other_name"], "https://docs.google.com/document/d/1zyN6ZNIWZx3qaPK3PPOd7H8wyW_LsxytOD7t7T8OTag/"),
+    (["rapid_fire_governance"], "https://docs.google.com/document/d/1vpDT4RJpSOw_vdGMdIv52l1zW7flywl9oqSMm6yPTW8/"),
 ]
