20e7218c9edeea5ec94c975de9a9f71e6bb5bf4a
Jordan Fisher <jordan.efisher@gmail.com>
2025-03-09 12:35:17 -0700

    Update doc list

--- doc_list.py
+++ doc_list.py
@@ -1,7 +1,10 @@
 gdoc_urls = [
     (["tiny_book_on_governance_of_machine"], "https://docs.google.com/document/d/1-CifFI15IsW5fj4DvSrH9SmpKJbAELyeHTKCfdGT6rs/"),
     (["the_corpse_still_warm"], "https://docs.google.com/document/d/1VnChDrRDJVqjMwu_kHS1BzhFjpn37ryNrRWZdaRUUxc/"),
+    (["please_be_silly"], "https://docs.google.com/document/d/1jKHxIXSFW1zwqVCSuRiTz8_svEtRXSaXqST-SAVe9_U/"),
+    (["governance_tl_dr", "https://docs.google.com/document/d/1ml52UUZ59ij4netSr_84YGlLdoVdzgqD_vT9ByNdRo0/"),
     (["implicit_guardrails"], "https://docs.google.com/document/d/1YSvUWGOTsk521pFGTIoaw6o375P-Hl5yDnh15dQ9MGg/"),
     (["by_any_other_name"], "https://docs.google.com/document/d/1zyN6ZNIWZx3qaPK3PPOd7H8wyW_LsxytOD7t7T8OTag/"),
     (["rapid_fire_governance"], "https://docs.google.com/document/d/1vpDT4RJpSOw_vdGMdIv52l1zW7flywl9oqSMm6yPTW8/"),
+    (["if_you_can_keep_it"], "https://docs.google.com/document/d/1zG0-u7hqdocWZAdLpTae0aaapAV41fgSFD9jUmjGxyI/"),
 ]
