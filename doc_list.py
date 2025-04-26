ab8026563aab81341db7866d5426dc25cc4e89fc
Jordan Fisher <jordan.efisher@gmail.com>
2025-04-26 11:42:37 -0700

    Update blog list

--- doc_list.py
+++ doc_list.py
@@ -1,11 +1,19 @@
 gdoc_urls = [
-    (["tiny_book_on_governance_of_machine"], "https://docs.google.com/document/d/1-CifFI15IsW5fj4DvSrH9SmpKJbAELyeHTKCfdGT6rs/"),
+    (["liberty_by_design"], "https://docs.google.com/document/d/1-CifFI15IsW5fj4DvSrH9SmpKJbAELyeHTKCfdGT6rs/"),
+
     (["superchecks_superbalances"], "https://docs.google.com/document/d/141zqb94MsAcwBFyk-srcTPEtN3dpDzJe6pN0wts_pcM/"),
     (["the_prompt_of_power"], "https://docs.google.com/document/d/1VnChDrRDJVqjMwu_kHS1BzhFjpn37ryNrRWZdaRUUxc/"),
-    (["please_be_silly"], "https://docs.google.com/document/d/1jKHxIXSFW1zwqVCSuRiTz8_svEtRXSaXqST-SAVe9_U/"),
-    (["governance_tl_dr"], "https://docs.google.com/document/d/1ml52UUZ59ij4netSr_84YGlLdoVdzgqD_vT9ByNdRo0/"),
     (["implicit_guardrails"], "https://docs.google.com/document/d/1YSvUWGOTsk521pFGTIoaw6o375P-Hl5yDnh15dQ9MGg/"),
     (["by_any_other_name"], "https://docs.google.com/document/d/1zyN6ZNIWZx3qaPK3PPOd7H8wyW_LsxytOD7t7T8OTag/"),
     (["rapid_fire_governance"], "https://docs.google.com/document/d/1vpDT4RJpSOw_vdGMdIv52l1zW7flywl9oqSMm6yPTW8/"),
     (["if_you_can_keep_it"], "https://docs.google.com/document/d/1zG0-u7hqdocWZAdLpTae0aaapAV41fgSFD9jUmjGxyI/"),
+    (["why_ai_is_accelerating"], "https://docs.google.com/document/d/1IV4hKVDoEgA5EZvDRfAtviDI0Stpj0m8MIEVPdLtLV4/"),
+    (["the_real_politik"], "https://docs.google.com/document/d/1rB0LTHYm4icUP0BCbc56X1VX9Oge5dTMQ8n0laPxi4Q/"),
+
+    (["please_be_silly"], "https://docs.google.com/document/d/1jKHxIXSFW1zwqVCSuRiTz8_svEtRXSaXqST-SAVe9_U/"),
+    (["concrete"], "https://docs.google.com/document/u/0/d/1c4bZTcHbjBJdIlbq9vmxS53SH04WQ__S-sSRNqXcLRg/"),
+    (["complexity_and_design"], "https://docs.google.com/document/d/1tgyr9L9hymcLlVvgkkF1Bfrho_DWo7jkW8--AeQi2K8/"),
+    (["crash_course_on_ai"], "https://docs.google.com/document/d/1g1MrziAeRAOkppKQvBdSgGJqC5d-pr4F5-i1mcRNJGQ/edit?tab=t.0"),
+
+    # ([""], ""),
 ]
