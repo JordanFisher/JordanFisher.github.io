6d1b267e2028c2296e260be175a5a1377c566fcb
Jordan Fisher <jordan.efisher@gmail.com>
2025-05-03 14:12:35 -0700

    Update the versions

--- liberty_by_design_versions.py
+++ liberty_by_design_versions.py
@@ -5,23 +5,27 @@ from dataclasses import dataclass
 class BookVersion:
     uri: str
     gdoc: str
-    description: str
+    user_choice: str = ""
 
 
 liberty_versions = [
     BookVersion(
-        uri="liberty_by_design",
+        uri="liberty_by_design_with_ai_intro",
         gdoc="https://docs.google.com/document/d/1-CifFI15IsW5fj4DvSrH9SmpKJbAELyeHTKCfdGT6rs/",
-        description="I'm new to AI"
+        user_choice="I'm new to AI"
     ),
     BookVersion(
         uri="liberty_by_design_up_to_speed_version",
         gdoc="https://docs.google.com/document/d/17jSp5YFfF2F-U2_vbWpYm7JUoy8iM2Z83WOHv5owuv0/",
-        description="I'm up to speed on AI"
+        user_choice="I'm up to speed on AI"
     ),
     BookVersion(
         uri="liberty_by_design_agi_pilled_version",
         gdoc="https://docs.google.com/document/d/1zrvgArp4Ulaf71Yz4mnFa9oCEQC5lB_CpVHH5G6bEik/",
-        description="I’m up to speed on AI and I think AGI is near"
+        user_choice="I’m up to speed on AI and I think AGI is really fucking near 🥵"
     ),
+    # BookVersion(
+    #     uri="liberty_by_design_physical_book_version",
+    #     gdoc="https://docs.google.com/document/d/1-CifFI15IsW5fj4DvSrH9SmpKJbAELyeHTKCfdGT6rs/",
+    # ),
 ]
